5e52c0c106632d3f58944d848fe40becaf6c1f32
Sukwon Oh <sukwon0709@gmail.com>
2013-02-23 20:09:12 +0000

    factor out JedisPubSub

--- redis-interface/app/controllers/Application.java
+++ redis-interface/app/controllers/Application.java
@@ -4,12 +4,20 @@ import play.*;
 import play.mvc.*;
 import play.libs.*;
 import play.libs.Json;
+import play.libs.F.Function;
+import play.libs.Akka;
+import static play.mvc.Results.async;
+import static akka.pattern.Patterns.ask;
+
+import akka.actor.*;
+
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ObjectNode;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import utils.Redis;
+import models.RedisSubscriber;
 import redis.clients.jedis.JedisPubSub;
 
 import java.io.IOException;
@@ -17,7 +25,8 @@ import java.util.HashMap;
 
 public class Application extends Controller {
 
-    private static HashMap<WebSocket.In<JsonNode>, JedisPubSub> PubSubMap = new HashMap<WebSocket.In<JsonNode>, JedisPubSub>();
+    private static HashMap<WebSocket.In<JsonNode>, RedisSubscriber> PubSubMap
+        = new HashMap<WebSocket.In<JsonNode>, RedisSubscriber>();
 
     @BodyParser.Of(BodyParser.Json.class)
     public static WebSocket<JsonNode> index() {
@@ -31,64 +40,18 @@ public class Application extends Controller {
                         final String channel = event.findPath("SUBSCRIBE").getTextValue();
                         System.out.println("Subscribing to " + channel);
 
-                        final JedisPubSub jPubSub = new JedisPubSub() {
-                            public void onMessage(String channel, String message) {
-                                ObjectNode reply = Json.newObject();
-                                ArrayNode body = reply.putArray("SUBSCRIBE");
-                                body.add("message");
-                                body.add(channel);
-                                body.add(message);
-                                out.write(reply);
-                            }
-
-                            public void onSubscribe(String channel, int subscribedChannels) {
-                                ObjectNode reply = Json.newObject();
-                                ArrayNode body = reply.putArray("SUBSCRIBE");
-                                body.add("subscribe");
-                                body.add(channel);
-                                body.add(subscribedChannels);
-                                out.write(reply);
-                            }
-
-                            public void onUnsubscribe(String channel, int subscribedChannels) {
-                                ObjectNode reply = Json.newObject();
-                                ArrayNode body = reply.putArray("SUBSCRIBE");
-                                body.add("unsubscribe");
-                                body.add(channel);
-                                body.add(subscribedChannels);
-                                out.write(reply);
-                            }
-
-                            public void onPSubscribe(String pattern, int subscribedChannels) {
-                                // TODO
-                            }
-
-                            public void onPUnsubscribe(String pattern, int subscribedChannels) {
-                                // TODO
-                            }
-
-                            public void onPMessage(String pattern, String channel, String message) {
-                                // TODO
-                            }
-                        };
-
-                        PubSubMap.put(in, jPubSub);
-
-                        //new Thread(new Runnable() {
-                        //   public void run() {
-                                Redis.getInstance().getJedis().subscribe(jPubSub, channel);
-                        //        System.out.println("return!");
-                        //    }
-                        //}).start();
+                        RedisSubscriber sub = new RedisSubscriber(in, out);
+                        PubSubMap.put(in, sub);
+                        Redis.getInstance().getJedis().subscribe(sub, channel);
                         System.out.println("hello");
                     }
                 });
 
                 in.onClose(new F.Callback0() {
                     public void invoke() {
-                        JedisPubSub jPubSub = PubSubMap.get(in);
-                        if (jPubSub != null) {
-                            jPubSub.unsubscribe();
+                        RedisSubscriber sub = PubSubMap.get(in);
+                        if (sub != null) {
+                            sub.unsubscribe();
                             System.out.println("unsubscribed!!");
                             PubSubMap.remove(in);
                         }
@@ -97,5 +60,4 @@ public class Application extends Controller {
             }
         };
     }
-  
 }
