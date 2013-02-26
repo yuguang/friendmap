f46f194c2e330a6e18a6e4c21b64cc56708380b0
Sukwon Oh <s3oh@uwaterloo.ca>
2013-02-26 14:59:27 -0500

    Websocket communication now works bi-directionally.

--- WebView/x86/o-g/.moc/moc_WebView.cpp
+++ WebView/x86/o-g/.moc/moc_WebView.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'WebView.hpp'
 **
-** Created: Sun Feb 24 02:16:25 2013
+** Created: Tue Feb 26 13:32:28 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.4)
 **
 ** WARNING! All changes made in this file will be lost!
